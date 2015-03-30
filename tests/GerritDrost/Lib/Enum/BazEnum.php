e6f258cac1c892230cc5be4eeef3d7008401bea2
Gerrit Drost <mail@gerritdrost.com>
2015-03-30 18:42:19 +0200

    Fixed unit tests

--- tests/GerritDrost/Lib/Enum/BazEnum.php
+++ tests/GerritDrost/Lib/Enum/BazEnum.php
@@ -9,10 +9,26 @@ class BazEnum extends Enum
 {
     const BAZ = 'baz';
 
+    private $initialized = false;
     private $bazValue;
 
     protected function __BAZ()
     {
         $this->bazValue = $this->getConstValue();
+    }
+
+    protected function __initEnum()
+    {
+        $this->initialized = true;
+    }
+
+    public function isInitialized()
+    {
+        return $this->initialized;
+    }
+
+    public function getBazValue()
+    {
+        return $this->bazValue;
     }
 }
